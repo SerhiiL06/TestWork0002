c643b6de5750a40ad23e337eb88e9323c2160c47
SerhiiL06 <sergiy06061997@gmail.com>
2025-04-26 17:53:00 +0300

    configurate alembic

---
+++ backend/infra/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    POSTGRES_DB: str
+    POSTGRES_USERNAME: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+
+
+env_config = Settings()
+
